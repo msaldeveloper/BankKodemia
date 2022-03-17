5a02285319dd9ea9caf7eb28562d58dd33a4e017
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-03-16 23:02:13 -0600

    first commit

---
+++ BankKodemia/BankKodemia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BankKodemia
+//
+//  Created by Mario Saldana on 16/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
