435d8c38e7f6425185a4c79290e0c7e8643b60d8
Juan de Dios <vargas.juandedios@icloud.com>
2022-03-16 23:53:59 -0600

    first commit juan

---
+++ BankKodemia/BankKodemia/Views/CreateAccountViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateAccountViewController.swift
+//  BankKodemia
+//
+//  Created by MacBook on 16/03/22.
+//
+
+import UIKit
+
+class CreateAccountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+}
