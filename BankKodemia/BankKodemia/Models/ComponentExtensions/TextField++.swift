661e1e6c3928564009715c1d7b7e929a0f76168f
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-03-16 23:06:59 -0600

    reorder proyect

---
+++ BankKodemia/BankKodemia/Models/ComponentExtensions/TextField++.swift
@@ -0,0 +1,24 @@
+//
+//  TextField++.swift
+//  Bankodemia
+//
+//  Created by Mario Saldana on 14/03/22.
+//
+
+import Foundation
+import UIKit
+
+extension UITextField{
+    func formatTextField(activate : Bool){
+        if activate == true {
+            self.textAlignment = .center
+            self.backgroundColor = .clear
+            self.layer.cornerRadius = 4
+            self.layer.borderColor = ConstantsCGColor.greenBlue
+            self.layer.borderWidth = 1
+        }else {
+            
+        }
+        
+    }
+}
