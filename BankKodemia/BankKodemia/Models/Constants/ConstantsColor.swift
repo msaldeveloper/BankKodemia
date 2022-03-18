d09d6dd58f7e5730dff432a73934867171efe1f6
Mario Alberto Saldana <marioalberto.saldana.contractor@bbva.com>
2022-03-17 19:17:10 -0600

    add first advances to login

--- BankKodemia/BankKodemia/Models/Constants/ConstantsColor.swift
+++ BankKodemia/BankKodemia/Models/Constants/ConstantsColor.swift
@@ -9,12 +9,14 @@ import Foundation
 import UIKit
 
 struct ConstantsCGColor{
+    static let clearBackground: CGColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
     static let greenBlue : CGColor = CGColor(red: 0, green: 204/255, blue: 204/255, alpha: 0.9)
     static let greyKodemia : CGColor = CGColor(red: 127/255, green: 127/255, blue: 127/255, alpha: 0.9)
 
 }
 
 struct ConstantsUIColor{
+    static let clearBackground: UIColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
     static let greenBlue : UIColor = UIColor(red: 0, green: 204/255, blue: 204/255, alpha: 0.9)
-    static let greyKodemia : UIColor = UIColor(red: 127/255, green: 127/255, blue: 127/255, alpha: 0.9
+    static let greyKodemia : UIColor = UIColor(red: 127/255, green: 127/255, blue: 127/255, alpha: 0.9)
 }
---
+++ BankKodemia/BankKodemia/Views/LoginViewController.swift
@@ -0,0 +1,72 @@
+//
+//  LoginViewController.swift
+//  BankKodemia
+//
+//  Created by Mario Saldana on 17/03/22.
+//
+
+import UIKit
+var arrowButton: UIButton?
+var initSessionLabel: UILabel?
+var initSessionWelcomeLabel: UILabel?
+var backgroundColor = ConstantsUIColor.clearBackground
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = backgroundColor
+        UIInit()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func UIInit(){
+        returnButton()
+        returnLabel()
+        welcomeLabel()
+//        emailField()
+//        passwordField()
+//        helpLabel()
+//        initSessionButton()
+    }
+    
+    func returnButton(){
+        arrowButton = UIButton()
+        arrowButton?.setImage(UIImage(named: "back"), for: .normal)
+        arrowButton?.backgroundColor = .red
+        arrowButton?.addTarget(self, action: #selector(backAction), for: .touchUpInside)
+        view.addSubview(arrowButton!)
+        arrowButton?.addAnchorsAndSize(width: nil, height: nil, left: 20.5, top: 90, right: nil, bottom: nil)
+    }
+    
+    func returnLabel(){
+        initSessionLabel = UILabel()
+        initSessionLabel?.text = TextLocals.init_session_top_message
+        initSessionLabel?.font = UIFont(name: "some", size: 14)
+        view.addSubview(initSessionLabel!)
+        initSessionLabel?.addAnchorsAndSize(width: 107, height: 21, left: 22.5, top: 85, right: nil, bottom: nil, withAnchor: .left, relativeToView: arrowButton)
+        //, withAnchor: <#T##Anchor?#>, relativeToView: <#T##UIView?#>
+    }
+    func welcomeLabel(){
+        initSessionWelcomeLabel = UILabel()
+        initSessionWelcomeLabel?.text = TextLocals.init_session_description_message
+        initSessionWelcomeLabel?.font = UIFont(name: "some", size: 16)
+        initSessionWelcomeLabel?.textAlignment = .left
+        initSessionWelcomeLabel?.lineBreakMode = .byWordWrapping
+        initSessionWelcomeLabel?.numberOfLines = 0
+        view.addSubview(initSessionWelcomeLabel!)
+        initSessionWelcomeLabel?.addAnchorsAndSize(width: nil, height: nil, left: 21, top: 24, right: 21, bottom: nil, withAnchor: .top, relativeToView: initSessionLabel)
+    }
+    
+    
+
+}
+// MARK: - OBJC Functions
+extension LoginViewController {
+    @objc func backAction(){
+        print("back button pressed")
+        dismiss(animated: true)
+    }
+}
