24bf752e374f2fad05f220ad363808c031696068
Juan de Dios <vargas.juandedios@icloud.com>
2022-03-23 15:41:16 -0600

    agregar vista Crear Contraseña

--- BankKodemia/BankKodemia/Utils/Constants/ConstansText.swift
+++ BankKodemia/BankKodemia/Utils/Constants/ConstansText.swift
@@ -145,6 +145,16 @@ struct Text{
             static let BottomMessage: String = "Conoce cómo protegemos tus documentos"
         }
         
+        struct CreatePassword {
+            static let TopMessage: String = "Crea una contraseña segura"
+            static let MediumMessage: String = "La usarás para entrar a la app y confirmar tus transacciones."
+            static let BottomMessage: String = "Usa al menos 6 caracteres alfanuméricos, no consecutivos ni repetidos."
+            static let CreateLabel: String = "Contraseña"
+            static let ConfirmLabel: String = "Confirma tu contraseña"
+            static let ButtonCreateMessage: String = "Crear Contraseña"
+            
+        }
+        
     }
 }
 
